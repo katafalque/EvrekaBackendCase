8aaa1bb2a067c5c143fdc8a335788b6d812a1a9f
katafalque <50772168+katafalque@users.noreply.github.com>
2022-08-01 22:52:56 +0300

    rename project.

--- src/evreka/wsgi.py
+++ src/evreka/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for evrekaqone project.
+WSGI config for evreka project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evrekaqone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evreka.settings')
 
 application = get_wsgi_application()
